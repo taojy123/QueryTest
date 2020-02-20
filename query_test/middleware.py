fc45d36874d3584beac3a9ddb3984d9bf89bef76
taojy123 <taojy123@163.com>
2020-02-20 21:56:47 +0800

    cors

--- query_test/middleware.py
+++ query_test/middleware.py
@@ -1,10 +1,24 @@
 
 from django.utils.deprecation import MiddlewareMixin
 
+CORS_ALLOW_HEADERS = [
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'cache-control',
+    'x-http-method-override',
+    'x-bulk-operation',
+    'x-frame-options',
+]
 
 class CorsMiddleware(MiddlewareMixin):
 
     def process_response(self, request, response):
         response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+        response['Access-Control-Allow-Headers'] = ', '.join(CORS_ALLOW_HEADERS)
+
         return response
 
